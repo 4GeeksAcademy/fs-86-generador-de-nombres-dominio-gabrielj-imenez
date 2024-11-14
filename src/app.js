f1c049a733e8b8f3ffa28852e4835b86e0e2e559
gabriel jimenez <gabo0813ajm@gmail.com>
2024-11-14 10:27:32 +0000

    dominios terminados

--- src/app.js
+++ src/app.js
@@ -9,3 +9,31 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'raccon'];
+
+for( let g of pronoun){
+  for( let m of adj){
+    for(let h of noun){
+      console.log(`${g}${m}${h}.com`);
+    }
+  }
+}
+
+
+let pronoun1 = ['the', 'our', 'your'];
+let adj1 = ['great', 'big', 'amazing'];
+let noun1 = ['jogger', 'raccon', 'fox'];
+let dominio1 = ['.com', '.es', '.co', '.net'];
+
+const newpronoun1 =  pronoun1.map( (pronoun2) => {
+  adj1.map( (adj2) => {
+    noun1.map( (noun2) => { 
+     dominio1.map( (dominio2) => {
+      console.log(`${pronoun2}${adj2}${noun2}${dominio2}`);   
+     });
+    });
+  } );
+});
